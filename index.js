9f1aae7a072b9964f1217b5c9cfb39121b508b61
MonoGot <kirati.samat@gmail.com>
2022-05-17 10:39:22 +0700

    test: add example code

---
+++ index.js
@@ -0,0 +1,17 @@
+exports.sayGoodMorning = async (req, res) => {
+  try {
+    if (!req.body.temp) {
+      throw "Temperature is undefined";
+    }
+    if (req.body.temp < 100) {
+      res.status(200).send("Temperature OK");
+    } else {
+      res.status(200).send("Too hot");
+    }
+    console.log("Hello World")
+  } catch (error) {
+    //return an error
+    console.log("got error: ", error);
+    res.status(500).send(error);
+  }
+ };
