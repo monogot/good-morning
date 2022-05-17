bbdf8201a7c0d33b8b2343e3410c340e4258786a
MonoGot <kirati.samat@gmail.com>
2022-05-17 13:42:58 +0700

    build: add packages

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+require('dotenv').config();
+
 exports.sayGoodMorning = async (req, res) => {
   try {
-    if (!req.body.temp) {
-      throw "Temperature is undefined";
-    }
-    if (req.body.temp < 100) {
-      res.status(200).send("Temperature OK");
-    } else {
-      res.status(200).send("Too hot");
-    }
-    console.log("Hello World")
+    // if (!req.body.temp) {
+    //   throw "Temperature is undefined";
+    // }
+
+    // When the client is ready, run this code (only once)
+    client.on('ready', () => {
+      console.log(`${client.user.tag} is online!`);
+
+      // const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+      console.log(client.channels);
+      // if (!channel) {
+      //   return console.error('Could not find the channel.');
+      // }
+
+      console.log('Found channel');
+
+      // channel.send('gm')
+      //   .then(message => console.log(`Sent message: ${message.content}`))
+      //   .catch(e => console.log(e));
+    });
+
+    // Login to Discord with your client's token
+    client.login(process.env.BOT_TOKEN);
   } catch (error) {
-    //return an error
-    console.log("got error: ", error);
+    console.log('got error: ', error);
     res.status(500).send(error);
   }
- };
+};
